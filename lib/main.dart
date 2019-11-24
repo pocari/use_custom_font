db4508c50a581aa45e7fb3064e646398d4778149
pocari <caffelattenonsugar@gmail.com>
2019-11-24 15:27:48 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('use custom font'),
+        ),
+        body: Center(
+          child: Text('hello, world'),
+        ),
+      );
+}
